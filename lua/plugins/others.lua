0e3834b57552caf7e3932a57d6d1137226973035
Jobin Nelson <jobinnelson369@gmail.com>
2023-11-11 23:06:39 +0530

    customize dashboard

---
+++ lua/plugins/others.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-treesitter/playground",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    cmd = "TSPlaygroundToggle",
+  },
+  {
+    "David-Kunz/gen.nvim",
+    event = "VeryLazy",
+  },
+}
