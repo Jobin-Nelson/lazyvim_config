adb28364bf3b1884463eb69f8a8d01b47dee016d
Jobin Nelson <jobinnelson369@gmail.com>
2024-01-02 11:24:08 +0530

    feat: make parity with default-nvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,7 +1,7 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-local my_group = vim.api.nvim_create_augroup("my_group", { clear = true })
+local my_group = vim.api.nvim_create_augroup("my_run_group", { clear = true })
 
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "python",
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,44 +2,43 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps hereby
 -- stylua: ignore
+local map = vim.keymap.set
 
 -- Telescope
-vim.keymap.set( 'n', '<leader>fd', '<cmd>lua require("config.custom.pickers").find_dotfiles()<cr>', { desc = "Find Dotfiles" })
-vim.keymap.set( 'n', '<leader>fz', '<cmd>lua require("config.custom.pickers").find_zoxide()<cr>', { desc = "Find Zoxide" })
-vim.keymap.set( 'n', '<leader>fs', '<cmd>lua require("config.custom.pickers").find_second_brain_files()<cr>', { desc = "Find Second brain files" })
-vim.keymap.set( 'n', '<leader>fO', '<cmd>lua require("config.custom.pickers").find_org_files()<cr>', { desc = "Find Org files" })
-vim.keymap.set( 'n', '<leader>fp', '<cmd>lua require("config.custom.pickers").find_projects()<cr>', { desc = "Find Projects" })
-vim.keymap.set( 'n', '<leader>fj', '<cmd>lua require("config.custom.pickers").find_journal()<cr>', { desc = "Find Journal" })
--- vim.keymap.set( 'n', '<leader>fe', '<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>', { desc = "File Browser (parent)" })
--- vim.keymap.set( 'n', '<leader>fE', '<cmd>Telescope file_browser<cr>', { desc = "File Browser (cwd)" })
-vim.keymap.set( 'n', '<leader>fb', '<cmd>Telescope buffers sort_mru=true<cr>', { desc = "Find Buffers" })
-vim.keymap.set( 'n', '<leader>fS', '<cmd>lua require("luasnip.loaders").edit_snippet_files()<cr>', { desc = "Find Snippets" })
-vim.keymap.set( 'n', '<leader>fg', '<cmd>Telescope emoji<cr>', { desc = "Find Emoji" })
-vim.keymap.set( 'n', '<leader>fG', '<cmd>Telescope glyph<cr>', { desc = "Find Glyphs" })
-vim.keymap.set( 'n', '<leader>gb', '<cmd>Telescope git_branches<cr>', { desc = "Git Branches" })
-vim.keymap.set( 'v', '<leader>gc', '<cmd>Telescope git_bcommits_range<cr>', { desc = "Git Buffer Commit Range" })
+map( 'n', '<leader>fd', '<cmd>lua require("config.custom.pickers").find_dotfiles()<cr>', { desc = "Find Dotfiles" })
+map( 'n', '<leader>fz', '<cmd>lua require("config.custom.pickers").find_zoxide()<cr>', { desc = "Find Zoxide" })
+map( 'n', '<leader>fs', '<cmd>lua require("config.custom.pickers").find_second_brain_files()<cr>', { desc = "Find Second brain files" })
+map( 'n', '<leader>fO', '<cmd>lua require("config.custom.pickers").find_org_files()<cr>', { desc = "Find Org files" })
+map( 'n', '<leader>fp', '<cmd>lua require("config.custom.pickers").find_projects()<cr>', { desc = "Find Projects" })
+map( 'n', '<leader>fj', '<cmd>lua require("config.custom.pickers").find_journal()<cr>', { desc = "Find Journal" })
+-- map( 'n', '<leader>fe', '<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>', { desc = "File Browser (parent)" })
+-- map( 'n', '<leader>fE', '<cmd>Telescope file_browser<cr>', { desc = "File Browser (cwd)" })
+map( 'n', '<leader>fb', '<cmd>Telescope buffers sort_mru=true<cr>', { desc = "Find Buffers" })
+map( 'n', '<leader>fS', '<cmd>lua require("luasnip.loaders").edit_snippet_files()<cr>', { desc = "Find Snippets" })
+map( 'n', '<leader>fg', '<cmd>Telescope emoji<cr>', { desc = "Find Emoji" })
+map( 'n', '<leader>fG', '<cmd>Telescope glyph<cr>', { desc = "Find Glyphs" })
+map( 'n', '<leader>gb', '<cmd>Telescope git_branches<cr>', { desc = "Git Branches" })
+map( 'v', '<leader>gc', '<cmd>Telescope git_bcommits_range<cr>', { desc = "Git Buffer Commit Range" })
 
 -- Buffers
-vim.keymap.set("n", "<b", "<cmd>BufferLineMovePrev<cr>", { desc = "Buffer Move Left" })
-vim.keymap.set("n", ">b", "<cmd>BufferLineMoveNext<cr>", { desc = "Buffer Move Right" })
-vim.keymap.set("n", "<leader>bc", "<cmd>BufferLinePickClose<cr>", { desc = "Buffer Close" })
-vim.keymap.set('n', '<leader>bk', '<cmd>call delete(expand("%:p")) <bar> bdelete!<cr>', { desc = "Buffer Kill" })
+map("n", "<b", "<cmd>BufferLineMovePrev<cr>", { desc = "Buffer Move Left" })
+map("n", ">b", "<cmd>BufferLineMoveNext<cr>", { desc = "Buffer Move Right" })
+map("n", "<leader>bc", "<cmd>BufferLinePickClose<cr>", { desc = "Buffer Close" })
+map('n', '<leader>bk', '<cmd>call delete(expand("%:p")) <bar> bdelete!<cr>', { desc = "Buffer Kill" })
 
 -- Terminal
--- vim.keymap.set('n', '<A-h>', '<cmd>ToggleTerm size=20 direction=horizontal<cr>', {desc = 'ToggleTerm horizontal split' })
--- vim.keymap.set('n', '<A-v>', '<cmd>ToggleTerm size=80 direction=vertical<cr>', {desc = 'ToggleTerm vertical split' })
--- vim.keymap.set('t', '<A-h>','<esc><cmd>ToggleTerm direction=horizontal<cr>', {desc = 'ToggleTerm horizontal split' })
--- vim.keymap.set('t', '<A-v>','<esc><cmd>ToggleTerm direction=vertical<cr>', {desc = 'ToggleTerm vertical split' })
-vim.keymap.set('t', '<C-w>','<C-\\><C-n><C-w>', {desc = 'Terminal window command' })
+map('t', '<C-w>','<C-\\><C-n><C-w>', {desc = 'Terminal window command' })
 
 -- Others
-vim.keymap.set('n', '<leader>rw', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = "Replace word" })
-vim.keymap.set('v', 'P', '"_dP', { silent = true, desc = "Paste without yanking" })
-vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc="Hover diagnostics" })
-vim.keymap.set('n', '<leader>we', '<cmd>lua require("config.custom.email_update").open()<cr>', { desc = "Send Email" })
-vim.keymap.set('n', '<leader>wt', '<cmd>lua require("config.custom.get_ticket").populate_ticket()<cr>', { desc = "Source Ticket" })
-vim.keymap.set('n', '<leader>jt', '<cmd>lua require("config.custom.org-tangle").tangle()<cr>', { desc = "Org Tangle" })
-vim.keymap.set('n', '<leader>jj', '<cmd>lua require("config.custom.utils").start_journal()<cr>', { desc = "Start Journal" })
-vim.keymap.set('n', '<leader>js', '<cmd>lua require("config.custom.utils").scratch_buffer()<cr>', { desc = "Scratch Buffer" })
-vim.keymap.set('n', '<leader>jr', '<cmd>lua require("config.custom.utils").rename_file()<cr>', { desc = "Rename File" })
-vim.keymap.set('n', '<leader>ji', '<cmd>lua require("config.custom.utils").set_indent()<cr>', { desc = "Set Indent" })
+map('n', '<leader>rw', ':%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>', { desc = "Replace word" })
+map('v', 'P', '"_dP', { silent = true, desc = "Paste without yanking" })
+map('n', 'gl', vim.diagnostic.open_float, { desc="Hover diagnostics" })
+map('n', '<leader>we', '<cmd>lua require("config.custom.email_update").open()<cr>', { desc = "Send Email" })
+map('n', '<leader>wt', '<cmd>lua require("config.custom.get_ticket").populate_ticket()<cr>', { desc = "Source Ticket" })
+map('n', '<leader>jt', '<cmd>lua require("config.custom.org-tangle").tangle()<cr>', { desc = "Org Tangle" })
+map('n', '<leader>jj', '<cmd>lua require("config.custom.utils").start_journal()<cr>', { desc = "Start Journal" })
+map('n', '<leader>js', '<cmd>lua require("config.custom.utils").scratch_buffer()<cr>', { desc = "Scratch Buffer" })
+map('n', '<leader>jr', '<cmd>lua require("config.custom.utils").rename_file()<cr>', { desc = "Rename File" })
+
+-- UI
+map('n', '<leader>ui', '<cmd>lua require("config.custom.utils").set_indent()<cr>', { desc = "Set Indent" })
